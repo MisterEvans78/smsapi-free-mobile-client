1ab38f2e2c6495f0c0a6c165556f1a64ede12140
Evans <71824258+MisterEvans78@users.noreply.github.com>
2023-07-23 11:23:40 +0200

    ajout client CLI

---
+++ smsapi-free-mobile-cli-client/smsapi-free-mobile-cli-client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+
+namespace SmsApiFreeMobile.CliClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string user, pass, msg;
+            if (args.Length > 2)
+            {
+                user = args[0];
+                pass = args[1];
+                msg = args[2];
+            }
+            else
+            {
+                Console.Write("Utilisateur : ");
+                user = Console.ReadLine() ?? string.Empty;
+                Console.Write("Clé d'identification : ");
+                pass = Console.ReadLine() ?? string.Empty;
+                Console.Write("Message : ");
+                msg = Console.ReadLine() ?? string.Empty;
+            }
+
+            SmsApi sms = new(user, pass, msg);
+            HttpStatusCode statusCode = await sms.CallApiAsync();
+
+            Console.WriteLine(SmsApi.DetermineMessage(statusCode));
+        }
+    }
+}
